2afbd44b28d1e7d68257c5addc65dc1b1e627d73
Yakov Didenko <kogotto@gmail.com>
2023-03-28 16:25:55 +0400

    Forbid curse copy

--- view/include/view/curse.h
+++ view/include/view/curse.h
@@ -8,6 +8,13 @@ public:
     Curse();
     ~Curse();
 
+    // Absolutely no sense in copies
+    Curse(const Curse&) = delete;
+    Curse& operator=(const Curse&) = delete;
+
+    Curse(Curse&&) = default;
+    Curse& operator=(Curse&&) = default;
+
     void print(const char* str);
     void refresh();
     int getchar();
